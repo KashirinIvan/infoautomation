b316d3b0e707bfb4929b943834c8f0fbcf7ad95b
Ivan <ivakashirin@yandex.ru>
2024-11-13 11:30:24 +0700

    add project 3 and 4

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -5,14 +5,13 @@ import styles from "./Projects.module.css"
 export const Projects = ({ projectList }) => {
     let { id } = useParams();
     const project = projectList.find((project) => project.id === Number(id));
-    const str = project.descriptionAll;
-    return (
-        <div>
-            <h3>{project.title}</h3>
-            <h4>Описание проекта</h4>
-            <p className={styles.descriptionAll}>{project.descriptionAll}</p>
-            <img className={styles.image} src={project.image} alt={`${project.title}`} />
-            <p>Год: {project.year}</p>
-        </div>
-    )
+        return (
+            <div>
+                <h3>{project.title}</h3>
+                <h4>Описание проекта</h4>
+                <p className={styles.descriptionAll}>{project.descriptionAll}</p>
+                <img className={styles.image} src={project.image} alt={`${project.title}`} />
+                <p>Год: {project.year}</p>
+            </div>
+        )
 }
